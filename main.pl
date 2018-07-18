c8dd1d1666cdcb13c83d4535a5ef98d12cc9b380
amintasvrp <amintas.pereira@ccc.ufcg.edu.br>
2018-07-17 22:38:07 -0300

    Adicionando calDamage: #4 e #5

---
+++ main.pl
@@ -0,0 +1,34 @@
+%
+%% IMPLEMENTAÇÕES FRONT
+%
+
+
+%
+%% IMPLEMENTAÇÕES BACK
+%
+
+% Ocorrências de vantagem (fatos)
+
+advantage("Mutant","Skill").
+advantage("Skill","Science").
+advantage("Science","Mystic").
+advantage("Mystic","Cosmic").
+advantage("Cosmic","Tech").
+advantage("Tech","Mutant").
+
+% Cálculo do dano do atacante, considerando a defesa do defensor e a relacao de vantagem entre classes
+
+calDamage(Damage, Defense, Atk, Def, Result) :- 
+    Adv is Damage / 4, 
+    calDamageAdvantage(Damage,Adv,Atk,Def,DamageAdv),
+    Result is DamageAdv - Defense.
+
+calDamageAdvantage(Damage,Adv,Atk,Def,DamageAdv) :-
+    advantage(Atk,Def) -> DamageAdv is Damage + Adv ; DamageAdv is Damage.
+
+
+
+
+
+
+
