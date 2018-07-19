0aaad37fe2e4ff51dbf6a26f721f02afdd348768
gabrielssa <gabriel.schubert.araujo@ccc.ufcg.edu.br>
2018-07-19 20:12:40 -0300

    adição dos métodos de verificação de vitória

--- main.pl
+++ main.pl
@@ -25,10 +25,18 @@ calDamage(Damage, Defense, Atk, Def, Result) :-
 
 calDamageAdvantage(Damage,Adv,Atk,Def,DamageAdv) :-
     advantage(Atk,Def) -> DamageAdv is Damage + Adv ; DamageAdv is Damage.
+	
+tamL([_], 1):- !.
+tamL([_|L], T) :- tamL(L, X), T is X+1.
 
+checkFinalVictory(P, R):-
+    tamL(P, T),
+    T =:= 12 -> R = true ; R = false.
 
+checkPartialVictory(P, R):-
+    P=< 0 -> R = true ; R = false.
 
-
-
+checkBattleLost(A,B,C,R):-
+    A =< 0 ; B =< 0 ; C =< 0 -> R = true ; R = false.
 
 
