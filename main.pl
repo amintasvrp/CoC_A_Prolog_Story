83652e1574076fedce985469300ed59bc83068ae
amintasvrp <amintas.pereira@ccc.ufcg.edu.br>
2018-07-19 23:52:08 -0300

    Revisão e Organização de Código

--- main.pl
+++ main.pl
@@ -2,12 +2,11 @@
 %% IMPLEMENTAÇÕES FRONT
 %
 
-
 %
-%% IMPLEMENTAÇÕES BACK
+%% FATOS 
 %
 
-% Ocorrências de vantagem (fatos)
+% Ocorrências de vantagem
 
 advantage("Mutant","Skill").
 advantage("Skill","Science").
@@ -15,6 +14,21 @@ advantage("Science","Mystic").
 advantage("Mystic","Cosmic").
 advantage("Cosmic","Tech").
 advantage("Tech","Mutant").
+
+% Condição de derrota
+battleLost(0,0,0).
+
+%
+%% IMPLEMENTAÇÕES DE MANIPULAÇÃO DE LISTAS
+%
+
+% Tamanho da lista
+tamL([_], 1):- !.
+tamL([_|L], Tamanho) :- tamL(L, X), Tamanho is X+1.
+
+%
+%% IMPLEMENTAÇÕES BACK
+%
 
 % Cálculo do dano do atacante, considerando a defesa do defensor e a relacao de vantagem entre classes
 
@@ -26,23 +40,16 @@ calDamage(Damage, Defense, Atk, Def, Result) :-
 calDamageAdvantage(Damage,Adv,Atk,Def,DamageAdv) :-
     advantage(Atk,Def) -> DamageAdv is Damage + Adv ; DamageAdv is Damage.
 
-% Auxiliar para cálculo do tamanho da lista sem erros.
-tamL([_], 1):- !.
-tamL([_|L], T) :- tamL(L, X), T is X+1.
-
 % Recebe a lista de campeões e verifica se ela está com todos os 12 (vitória).
 checkFinalVictory(Party, Result):-
     tamL(Party, Lenght),
     Lenght =:= 12 -> Result = true ; Result = false.
 
-% Recebe o HP do campeão inimigo e retorna true se ele estiver morto.
+% Recebe o HP do campeão inimigo e verifica se ele estiver morto.
 checkPartialVictory(ChampionHP, Result):-
     ChampionHP=< 0 -> Result = true ; Result = false.
 
-% Fatos do battlelost
-battleLost(0,0,0).
-
-% Recebe os três HPS dos campeões da party e retorna true se todos estiverem mortos
+% Recebe os três HPS dos campeões da party e verifica se todos estão derrotados
 checkBattleLost(ChampHP1,ChampHP2,ChampHP3,Result):-
     battleLost(ChampHP1,ChampHP2,ChampHP3) -> Result = true ; Result = false.
 
