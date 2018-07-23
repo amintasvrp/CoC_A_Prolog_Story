38077fe3141e92b9c64d3fb2f3102bddab70985a
ItamarFarias <itamar.farias@ccc.ufcg.edu.br>
2018-07-23 20:33:48 -0300

    Add files via upload

---
+++ main.pl
@@ -0,0 +1,8 @@
+:- initialization(menu).
+menu :- 
+write('Welcome to the Colosseum of Champions!!!'), nl,
+write('Get ready for a great adventure that could'), nl, 
+write('result in your glory...'), nl,
+write('...or in your oblivion...'), nl,
+
+write('\nDo you want to play?\n(1) Yes\n(2) No\nOption'), read(Nome).
